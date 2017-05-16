d191edf86e8b532f78718b411fe8171b4f1306ff
Lukas Quast <lukas.quast@ort-online.net>
2017-05-16 12:10:20 +0200

    fixed issue with nested wheres and added tests

--- src/Mpociot/Couchbase/Query/Builder.php
+++ src/Mpociot/Couchbase/Query/Builder.php
@@ -156,9 +156,20 @@ class Builder extends BaseBuilder
     {
         $this->from = $this->connection->getBucketName();
         $this->type = $type;
-
+        
         $this->where('eloquent_type', $type);
         return $this;
+    }
+    
+    /**
+     * Create a new query instance for nested where condition.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function forNestedWhere()
+    {
+        // ->from($this->from) is wrong, and ->from($this->type) is redundant in nested where
+        return $this->newQuery();
     }
 
     /**
