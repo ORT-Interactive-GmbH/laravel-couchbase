7bc7a7bcd01590e73084d6aeba4df0f0a97cb897
Richard H <rh@hrdns.de>
2018-01-24 11:30:30 +0100

    fix missing use statement

--- src/Mpociot/Couchbase/Relations/HasMany.php
+++ src/Mpociot/Couchbase/Relations/HasMany.php
@@ -1,5 +1,6 @@
 <?php namespace Mpociot\Couchbase\Relations;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany as EloquentHasMany;
 
