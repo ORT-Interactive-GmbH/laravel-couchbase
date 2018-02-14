c7998775f94cc4af6d34964ce63556b0a944ebc4
Lukas Quast <lukas.quast@ort-online.net>
2018-02-14 14:49:39 +0100

    added gitlab-ci integration

--- tests/config/database.php
+++ tests/config/database.php
@@ -1,28 +1,37 @@
 <?php
-
 return [
-
     'connections' => [
-
-        'couchbase' => [
+        'couchbase' => strpos(env('CB_VERSION', '4') , '4') === 0 ? [
             'name'       => 'couchbase',
             'driver'     => 'couchbase',
-            'port'       => '8091',
-            'host'       => '127.0.0.1',
-            'bucket'     => 'testing',
-            'n1ql_hosts' => ['http://127.0.0.1:8093']
+            'port'       => env('CB_PORT', 8091),
+            'host'       => env('CB_HOST', '127.0.0.1'),
+            'bucket'     => env('CB_DATABASE', env('CB_BUCKET', 'test-ing')),
+            'user'       => env('CB_USER', env('CB_USERNAME')),
+            'password'   => env('CB_PASSWORD'),
+            'n1ql_hosts' => ['http://127.0.0.18093'],
+        ] : [
+            'name'       => 'couchbase',
+            'driver'     => 'couchbase',
+            'port'     => env('CB_PORT', 8093),
+            'host'     => env('CB_HOST', '127.0.0.1'),
+            'bucket'   => env('CB_DATABASE', env('CB_BUCKET', 'test-ing')),
+            'username' => env('CB_USER', env('CB_USERNAME', 'dbuser_backend')),
+            'password' => env('CB_PASSWORD', 'password_backend'),
+            'auth_type' => env('CB_AUTH_TYPE', \Mpociot\Couchbase\Connection::AUTH_TYPE_USER_PASSWORD),
+            'admin_username' => env('CB_ADMIN_USERNAME',env('CB_USER',env('CB_USERNAME','Administrator'))),
+            'admin_password' => env('CB_ADMIN_PASSWORD',env('CB_PASSWORD','password')),
         ],
-
-        'mysql' => [
+        'mysql'     => [
+            'name'      => 'mysql',
             'driver'    => 'mysql',
-            'host'      => '127.0.0.1',
+            'host'      => env('MYSQL_HOST', '127.0.0.1'),
             'database'  => 'testing',
-            'username'  => 'root',
-            'password'  => '',
+            'username'  => env('MYSQL_USER', 'root'),
+            'password'  => env('MYSQL_PASSWORD', env('MYSQL_ROOT_PASSWORD')),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
         ],
     ],
-
 ];
